8b00d09a919e7320197bab5770602d6be42c3809
Angelo <angelolencina@gmail.com>
2020-07-27 21:32:11 -0300

    initial commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import ButtonLink from './components/ButtonLink'
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="lencinaflix logo"/>
+      </a>
+      <ButtonLink className="ButtonLink" href="/">
+        Novo vídeo
+      </ButtonLink>
+    </nav>
+  );
+}
+
+export default Menu
