07ae4681a602a2dc3cfcd463298944cd64bed1d3
Angelo <angelolencina@gmail.com>
2020-07-28 23:05:11 -0300

    adicionado router no index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import './reset.css';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+     <Route path="/" component={Home} exact/>
+     <Route path="/cadastro/video" component={CadastroVideo} />
+     <Route path="/cadastro/categoria" component={CadastroCategoria} exact/>
+
+     <Route  component={() => <div>Página 404</div> }/>
+
+    </Switch>
+  </BrowserRouter>,
+  
   document.getElementById('root')
 );
